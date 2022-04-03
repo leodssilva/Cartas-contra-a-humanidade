2c355e4b1e43e60d243c46a71770aea1e88dd9c6
leodssilva <leosilva2709@gmail.com>
2022-04-03 11:22:52 -0300

    Primeira versão do jogo

---
+++ script/script_brancas.js
@@ -0,0 +1,43 @@
+let btnWhite = document.getElementById('btn-brancas')
+let btnClean = document.getElementById('btn-limpar')
+let btnSwap = document.getElementById('troca')
+
+let divCards = document.getElementById('cartas')
+
+function addWhite() {
+
+    for (let i = 1; i <= 6; i++) {
+        const random = Math.floor(Math.random() * 219) + 1;
+
+        var image = document.createElement("img");
+
+        image.setAttribute("src", `../images/brancas/branca (${random}).jpg`);
+        image.setAttribute("onclick", "deleteImage(this)");
+        
+        divCards.appendChild(image)
+        
+    }
+    
+}
+
+function deleteImage(el) {
+    let element = el
+    element.remove();
+}
+
+function swapImages() {
+    let firstImage = divCards.firstElementChild
+    divCards.firstElementChild.remove()
+    divCards.appendChild(firstImage)
+}
+
+btnWhite.addEventListener('click', addWhite)
+btnClean.addEventListener('click', () => {
+    divCards.innerHTML = ""
+})
+btnSwap.addEventListener('click', swapImages)
+
+
+
+
+
---
+++ script/script_preta.js
@@ -0,0 +1,24 @@
+let btnBlack = document.getElementById('btn-preta')
+let btnClean = document.getElementById('btn-limpar')
+
+let divCards = document.getElementById('cartas')
+
+function addBlack() {
+
+        const random = Math.floor(Math.random() * 99) + 1;
+
+        var image = document.createElement("img");
+
+        image.setAttribute("src", `../images/pretas/preta (${random}).jpg`);
+        image.setAttribute("onclick", "deleteImage(this)");
+        
+        divCards.appendChild(image)
+    
+}
+
+function deleteImage(el) {
+    let element = el
+    element.remove();
+}
+
+btnBlack.addEventListener('click', addBlack)
